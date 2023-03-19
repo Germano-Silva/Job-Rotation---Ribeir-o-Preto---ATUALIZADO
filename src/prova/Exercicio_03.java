20c1b9f4df7896036e35742194a16f396412cd54
Germano Silva <germano.silva@ufn.edu.br>
2023-03-19 10:48:00 -0300

    Update Exercicio_03.java

--- src/prova/Exercicio_03.java
+++ src/prova/Exercicio_03.java
@@ -4,73 +4,87 @@ import java.io.File;
 import com.google.gson.Gson;
 import java.util.Scanner;
 
+/**
+ * 3 Dado um vetor que guarda o valor de faturamento diário de uma
+ * distribuidora, faça um programa, na linguagem que desejar, que calcule e
+ * retorne: • O menor valor de faturamento ocorrido em um dia do mês; • O maior
+ * valor de faturamento ocorrido em um dia do mês; • Número de dias no mês em
+ * que o valor de faturamento diário foi superior à média mensal.
+ * 
+ * IMPORTANTE: a) Usar o json ou xml disponível como fonte dos dados do
+ * faturamento mensal; b) Podem existir dias sem faturamento, como nos finais de
+ * semana e feriados. Estes dias devem ser ignorados no cálculo da média;
+ * 
+ * @author Germano-Silva
+ *
+ */
+
 public class Exercicio_03 {
 
 	public static void main(String[] args) {
-		 double[] faturamento = null;
-		 double maior;
-		 double menor;
-		 double soma;
-		 int diasUteis;
-		 double media;
-		 int acimaDaMedia;
-		 
-	        File arquivo = new File("faturamento.json");
-	        String conteudo = "";
-	        try {
-	            Scanner scan = new Scanner(arquivo);
-	            while (scan.hasNextLine()) {
-	                conteudo += scan.nextLine();
-	            }
-	            scan.close();
+		double[] faturamento = null;
+		double maior;
+		double menor;
+		double soma;
+		int diasUteis;
+		double media;
+		int acimaDaMedia;
 
-	            
-	            faturamento = converterJsonParaFaturamento(conteudo);
-	        } catch (Exception e) {
-	            System.out.println("Erro ao ler o arquivo de faturamento!");
-	            return;
-	        }
+		File arquivo = new File("faturamento.json");
+		String conteudo = "";
+		try {
+			Scanner scan = new Scanner(arquivo);
+			while (scan.hasNextLine()) {
+				conteudo += scan.nextLine();
+			}
+			scan.close();
 
-	        maior = faturamento[0];
-	        menor = faturamento[0];
-	        
-	        soma = 0.0;
-	        diasUteis = 0;
+			faturamento = converterJsonParaFaturamento(conteudo);
+		} catch (Exception e) {
+			System.out.println("Erro ao ler o arquivo de faturamento!");
+			return;
+		}
 
-	        for (int i = 0; i < faturamento.length; i++) {
-	            double valor = faturamento[i];
-	            if (valor > 0) {
-	                soma += valor;
-	                diasUteis++;
-	            }
+		maior = faturamento[0];
+		menor = faturamento[0];
 
-	            if (valor < menor) {
-	                menor = valor;
-	            }
+		soma = 0.0;
+		diasUteis = 0;
 
-	            if (valor > maior) {
-	                maior = valor;
-	            }
-	        }
+		for (int i = 0; i < faturamento.length; i++) {
+			double valor = faturamento[i];
+			if (valor > 0) {
+				soma += valor;
+				diasUteis++;
+			}
 
-	        media = soma / diasUteis;
+			if (valor < menor) {
+				menor = valor;
+			}
 
-	        acimaDaMedia = 0;
-	        for (int i = 0; i < faturamento.length; i++) {
-	            if (faturamento[i] > media) {
-	                acimaDaMedia++;
-	            }
-	        }
+			if (valor > maior) {
+				maior = valor;
+			}
+		}
 
-	        System.out.println("Menor faturamento diário: R$" + menor);
-	        System.out.println("Maior faturamento diário: R$" + maior);
-	        System.out.println("Número de dias com faturamento acima da média: " + acimaDaMedia);
-	    }
+		media = soma / diasUteis;
 
-	    public static double[] converterJsonParaFaturamento(String json) {
-	    	Gson gson = new Gson();
-	        double[] faturamento = gson.fromJson(json, double[].class);
-	        return faturamento;
+		acimaDaMedia = 0;
+		for (int i = 0; i < faturamento.length; i++) {
+			if (faturamento[i] > media) {
+				acimaDaMedia++;
+			}
+		}
+
+		System.out.println("Menor faturamento diário: R$" + menor);
+		System.out.println("Maior faturamento diário: R$" + maior);
+		System.out.println("Número de dias com faturamento acima da média: " + acimaDaMedia);
+	}
+
+	public static double[] converterJsonParaFaturamento(String json) {
+		Gson gson = new Gson();
+		double[] faturamento = gson.fromJson(json, double[].class);
+		return faturamento;
 
 	}
 
